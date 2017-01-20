bb26b5d4b2565641a27e81ee3114c5e8e22afff3
devdtb <devdtb@gmail.com>
2017-01-20 14:44:43 +0100

    Setup repo

---
+++ src/main/java/dtb/ui/ProductUiApplication.java
@@ -0,0 +1,12 @@
+package dtb.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductUiApplication.class, args);
+	}
+}
