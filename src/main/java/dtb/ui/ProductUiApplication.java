41c859f26671a42423d9db23940df9bbfdb2f5e2
dtb <dtb@pc-dtb>
2017-01-22 11:16:09 +0100

    Setup lpt

--- src/main/java/dtb/ui/ProductUiApplication.java
+++ src/main/java/dtb/ui/ProductUiApplication.java
@@ -3,6 +3,11 @@ package dtb.ui;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ * @author dtb
+ *
+ */
 @SpringBootApplication
 public class ProductUiApplication {
 
